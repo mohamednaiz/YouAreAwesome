7f41297af3453af0dc29efc1365fb4d9cda363e6
Mohamed Naiz <mohamed.naiz@outlook.com>
2025-01-10 14:37:29 +0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Mohamed Naiz on 10-1-25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
